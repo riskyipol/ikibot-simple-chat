4c4a3cebac866c1c7cc4ef5983047a691fa19ae6
riskypratama <38471180+riskyipol@users.noreply.github.com>
2025-12-17 07:26:54 +0700

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,59 @@
+const pertanyaan = document.getElementById("pertanyaan");
+const jawaban = document.getElementById("jawaban");
+const loaders = document.getElementById("loaders");
+const container = document.getElementsByClassName("container")
+
+let init = 0;
+
+const botSay = (data) => {
+  return ["Halo, Perkenalkan nama saya ikibot. siapa nama kamu?",
+    `Halo ${data?.nama}, berapa usia kamu?`,
+    `Ohhh ${data?.usia}, hobi kamu apa ya?`,
+    `waw sama dong aku juga hobi ${data?.hobi}, btw punya pacar gak?`,
+    `ohh ${data?.pacar}, yaudah kalo gitu. udahan yah`
+  ];
+};
+
+pertanyaan.innerHTML = botSay()[0];
+
+let usersData = []
+
+function botStart() {
+    if(jawaban.value.length < 1) return alert("silahkan isi jawaban dulu")
+  init++;
+  if (init === 1) {
+    botDelay({nama: jawaban.value})
+  }else if (init === 2) {
+    botDelay({usia: jawaban.value})
+  }else if (init === 3) {
+    botDelay({hobi: jawaban.value})
+  }else if (init === 4) {
+    botDelay({pacar: jawaban.value})
+  }else if (init === 5) {
+    finishing()
+  }else {
+    botEnd()
+  }
+}
+
+function botDelay(jawabanUser) {
+  loaders.style.display = "block"
+  container[0].style.filter = "blur(8px)"
+    setTimeout(() => {
+        pertanyaan.innerHTML = botSay(jawabanUser)[init]
+        loaders.style.display = "none"
+        container[0].style.filter = "none"
+    }, 1000);
+    usersData.push(jawaban.value)
+    jawaban.value = ""
+}
+
+function finishing() {
+    pertanyaan.innerHTML = `Thank u ya ${usersData[0]} udah main ke ikibot, lain kali kita main ${usersData[2]} bareng ya `
+    jawaban.value = "sama-sama yaa"
+}
+
+function botEnd() {
+    alert(`Terima kasih ${usersData[0]} sudah berkunjung, anda akan kembali ke halaman utama`)
+    window.location.reload()
+}
